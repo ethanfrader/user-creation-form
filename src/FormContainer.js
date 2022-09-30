18c6b64fd5c5f6616646a938d2205a748b20dafe
Ethan Rader <61254527+ethanfrader@users.noreply.github.com>
2022-09-30 12:54:49 -0500

    safe nav and congrats page

--- src/FormContainer.js
+++ src/FormContainer.js
@@ -44,6 +44,9 @@ export class FormContainer extends Component {
             passwordError: false,
             occupationError: false,
             stateError: false,
+            disableButton: false,
+            titleMessage: 'Please fill out this form to create your account:',
+            congrats: false
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -96,11 +99,35 @@ export class FormContainer extends Component {
         elements.forEach(el => {
             this.validateElementByName(el.name, el.value);
         })
-        if(this.state.fullNameError || this.state.emailError || this.state.passwordError ||
-            this.state.occupationError || this.state.stateError) {
-                
-        } else {
-            console.log('looks good')
+        if((this.state.fullNameError && this.state.emailError && this.state.passwordError &&
+            this.state.occupationError && this.state.stateError) === false && (this.state.fullName
+            && this.state.email && this.state.password && this.state.occupation && this.state.state)) {
+                const bodyObj = {
+                    name: this.state.fullName,
+                    email: this.state.email,
+                    password: this.state.password,
+                    occupation: this.state.occupation,
+                    state: this.state.state
+                }
+                const reqOptions = {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(bodyObj)
+                }
+                this.setState({disableButton : true});
+                fetch('https://frontend-take-home.fetchrewards.com/form', reqOptions)
+                    .then(res => {
+                        if(res.status === 200) {
+                            let partialState = {};
+                            partialState.congrats = true;
+                            partialState.titleMessage = 'Congrats!';
+                            this.setState(partialState);
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        this.setState({disableButton : false})
+                    })
         }
     }
 
@@ -113,67 +140,75 @@ export class FormContainer extends Component {
                             <div class="logo">
                                 <img class="logo" src={require("./icon.png")}></img>
                             </div>
-                            <h1>Please fill out this form to create your account:</h1>
+                            <h1>{this.state.titleMessage}</h1>
                         </div>
                     </div>
-                    <div class="form-control">
-                        <div>
-                            <label for="fullName">Full Name</label>
-                            <input type="text" 
-                                    id="fullName" 
-                                    name="fullName"
-                                    class={`${this.state.fullNameError ? 'error' : ''}`} 
-                                    onChange={this.handleChange}
-                                    onBlur={this.validateInput}></input>
-                            <ErrorText message="Full name is required" show={this.state.fullNameError}/>
+                    <div>
+                        {!this.state.congrats && 
+                        <div class="form-control">
+                            <div>
+                                <label for="fullName">Full Name</label>
+                                <input type="text" 
+                                        id="fullName" 
+                                        name="fullName"
+                                        class={`${this.state.fullNameError ? 'error' : ''}`} 
+                                        onChange={this.handleChange}
+                                        onBlur={this.validateInput}></input>
+                                <ErrorText message="Full name is required" show={this.state.fullNameError}/>
+                            </div>
+                            <div>
+                                <label for="email">Email</label>
+                                <input type="text" 
+                                        id="email" 
+                                        name="email"
+                                        class={`${this.state.emailError ? 'error' : ''}`}  
+                                        onChange={this.handleChange}
+                                        onBlur={this.validateInput}></input>
+                                <ErrorText message="Email is required" show={this.state.emailError}/>
+                            </div>
+                            <div>
+                                <label for="password">Password</label>
+                                <input type="password" 
+                                        id="password" 
+                                        name="password" 
+                                        class={`${this.state.passwordError ? 'error' : ''}`} 
+                                        onChange={this.handleChange}
+                                        onBlur={this.validateInput}></input>
+                                <ErrorText message="Password must be at least 8 characters" show={this.state.passwordError}/>
+                            </div>
+                            <div>
+                                <label for="occupation">Occupation</label>
+                                <select id="occupation" 
+                                        name="occupation" 
+                                        class={`${this.state.occupationError ? 'error' : ''}`} 
+                                        onChange={this.handleChange}
+                                        onBlur={this.validateInput}>
+                                    <option value="">Select your occupation</option>
+                                    <ListOccupations occupations={this.state.occupations} />
+                                </select>
+                                <ErrorText message="You must select an occupation" show={this.state.occupationError}/>
+                            </div>
+                            <div>
+                                <label for="state">State</label>
+                                <select id="state" 
+                                        name="state" 
+                                        class={`${this.state.stateError ? 'error' : ''}`} 
+                                        onChange={this.handleChange}
+                                        onBlur={this.validateInput}>
+                                    <option value="">Select your state</option>
+                                    <ListStates states={this.state.states} />
+                                </select>
+                                <ErrorText message="You must select your state" show={this.state.stateError}/>
+                            </div>
+                            <div class="button">
+                                <button disabled={this.state.disableButton} onClick={this.handleSubmit}>Sign Up</button>
+                            </div>
+                        </div> }
+                        {this.state.congrats && 
+                        <div class="form-control">
+                            <h4>Your account has successfully been created. Feel free to navigate away form this page.</h4>
                         </div>
-                        <div>
-                            <label for="email">Email</label>
-                            <input type="text" 
-                                    id="email" 
-                                    name="email"
-                                    class={`${this.state.emailError ? 'error' : ''}`}  
-                                    onChange={this.handleChange}
-                                    onBlur={this.validateInput}></input>
-                            <ErrorText message="Email is required" show={this.state.emailError}/>
-                        </div>
-                        <div>
-                            <label for="password">Password</label>
-                            <input type="password" 
-                                    id="password" 
-                                    name="password" 
-                                    class={`${this.state.passwordError ? 'error' : ''}`} 
-                                    onChange={this.handleChange}
-                                    onBlur={this.validateInput}></input>
-                            <ErrorText message="Password must be at least 8 characters" show={this.state.passwordError}/>
-                        </div>
-                        <div>
-                            <label for="occupation">Occupation</label>
-                            <select id="occupation" 
-                                    name="occupation" 
-                                    class={`${this.state.occupationError ? 'error' : ''}`} 
-                                    onChange={this.handleChange}
-                                    onBlur={this.validateInput}>
-                                <option value="">Select your occupation</option>
-                                <ListOccupations occupations={this.state.occupations} />
-                            </select>
-                            <ErrorText message="You must select an occupation" show={this.state.occupationError}/>
-                        </div>
-                        <div>
-                            <label for="state">State</label>
-                            <select id="state" 
-                                    name="state" 
-                                    class={`${this.state.stateError ? 'error' : ''}`} 
-                                    onChange={this.handleChange}
-                                    onBlur={this.validateInput}>
-                                <option value="">Select your state</option>
-                                <ListStates states={this.state.states} />
-                            </select>
-                            <ErrorText message="You must select your state" show={this.state.stateError}/>
-                        </div>
-                        <div class="button">
-                            <button onClick={this.handleSubmit}>Sign Up</button>
-                        </div>
+                        }
                     </div>
                 </div>
 
